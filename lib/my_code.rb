a0eee55d2c202203d8be1ff409c8274f6ba147f4
Richard Bobo <richard.bobo@gmail.com>
2020-03-07 20:46:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,10 +2,11 @@ def map(source_array)
  source_array.map { |i| -i }
 end
 
+map(source_array) { |i| i*2 }
 
-def map_to_double(source_array)
-  source_array.map { |i| i*2 }
-end
+# def map_to_double(source_array)
+#   source_array.map { |i| i*2 }
+# end
 
 
 # map_to_double(dune) { |i| -i*2 }
