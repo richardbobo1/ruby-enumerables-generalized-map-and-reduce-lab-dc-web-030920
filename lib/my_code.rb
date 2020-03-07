9efba0b593dd1b6320b4f8c20cceec7a3452ba6a
Richard Bobo <richard.bobo@gmail.com>
2020-03-07 20:40:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,13 @@
-def my_own_map(array)
-  counter = 0 
-  while counter < array.length 
-    array[counter] = array[counter]*-1
-    counter += 1 
-  end 
-  array 
-end 
+
+
+
+# def my_own_map(array)
+#   counter = 0 
+#   while counter < array.length 
+#     array[counter] = array[counter]*-1
+#     counter += 1 
+#   end 
+#   array 
+# end 
 
   
