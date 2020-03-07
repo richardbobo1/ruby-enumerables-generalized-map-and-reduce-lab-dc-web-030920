57f3f63c8265bee487e59a2945d717e5ffb13f21
Richard Bobo <richard.bobo@gmail.com>
2020-03-07 20:51:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,21 @@
 def map(source_array)
- source_array.map { |i| -i }
+  new_array = []
+  counter = 0 
+  while counter < source_array.length 
+    new.push(yield(source_array[counter]))
+    counter += 1 
+  end
+  new_array
 end
 
-def map2(source_array)
-source_array.map { |i| i*2 }
-end
+
+# def map(source_array)
+# source_array.map { |i| -i }
+# end
+
+# def map2(source_array)
+# source_array.map { |i| i*2 }
+# end
 
 # def map_to_double(source_array)
 #   source_array.map { |i| i*2 }
