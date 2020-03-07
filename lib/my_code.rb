a5c64af6b8724660d53ff35ab927d5955f0fa79e
Richard Bobo <richard.bobo@gmail.com>
2020-03-07 21:01:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,11 @@
-def map(source_array)
-  new_array = []
+def map(array)
+  array = []
   counter = 0 
-  while counter < source_array.length 
-    new.push(yield(source_array[counter]))
+  while counter < array.length 
+    new.push(yield(array[counter]))
     counter += 1 
   end
-  new_array
+  array 
 end
 
 def reduce(array, sv=nil)
@@ -23,18 +23,3 @@ def reduce(array, sv=nil)
 end 
 sum 
 end 
-
-# def map(source_array)
-# source_array.map { |i| -i }
-# end
-
-# def map2(source_array)
-# source_array.map { |i| i*2 }
-# end
-
-# def map_to_double(source_array)
-#   source_array.map { |i| i*2 }
-# end
-
-
-# map_to_double(dune) { |i| -i*2 }
